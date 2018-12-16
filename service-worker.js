c5ba6bb72bb2b5ed800b56c6f823c3dc8b6b7af3
dewankpant <dewankpant@gmail.com>
2018-12-16 04:22:17 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -4,7 +4,10 @@ self.addEventListener('push', function (event) {
     // Extract payload as JSON object, default to empty object
     var data = event.data.json() || {};
 
+    
+      
 
+    
 
     // Extract notification image URL
     var image = data.image || 'https://sdk.pushy.me/web/assets/img/icon.png';
@@ -36,13 +39,36 @@ self.addEventListener("show", function(e){
 // Listen for notification click event
 self.addEventListener('notificationclick', function (event) {
 
-    
+    var s_c = '{"username":"dewankpant",\
+"contexts":[{"ip":"293.168.1.1",\
+    "charge_status":"Not charging",\
+    "Location_lat": "32.73",\
+    "Location_lng":"78.34",\ 
+    "Battery level":"73",\ 
+    "OS":"Android 9.0",\ 
+    "CPU": "Snapdragon 845"\
+}],\ 
+"device_type":"chrome"\
+}'
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "POST", "http://52.15.230.213:5000/contexthandler", false ); 
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(JSON.stringify(s_c));
+    console.log(xmlHttp.responseText);
 
 
    
 
     // Attempt to extract notification URL
+<<<<<<< HEAD
     var url = "https://www.facebook.com";
+=======
+    var url = event.notification.data.url;
+
+    // Check if it exists
+    if (url) {
+        // Open the target URL in a new tab/window
+>>>>>>> parent of 4be2a1c... Update service-worker.js
         event.waitUntil(clients.openWindow(url));
     
 });
