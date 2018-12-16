4be2a1ca634e9cb1f9c6e95013b8b1813f177b69
dewankpant <dewankpant@gmail.com>
2018-12-16 03:32:53 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,10 +6,7 @@ self.addEventListener('push', function (event) {
     // Extract payload as JSON object, default to empty object
     var data = event.data.json() || {};
 
-    
-      
 
-    
 
     // Extract notification image URL
     var image = data.image || 'https://sdk.pushy.me/web/assets/img/icon.png';
@@ -35,29 +32,14 @@ self.addEventListener('push', function (event) {
 // Listen for notification click event
 self.addEventListener('notificationclick', function (event) {
 
-    var s_c = '{"username":"dewankpant",\
-"contexts":[{"ip":"293.168.1.1",\
-    "charge_status":"Not charging",\
-    "Location_lat": "32.73",\
-    "Location_lng":"78.34",\ 
-    "Battery level":"73",\ 
-    "OS":"Android 9.0",\ 
-    "CPU": "Snapdragon 845"\
-}],\ 
-"device_type":"chrome"\
-}'
-    var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open( "POST", "http://52.15.230.213:5000/contexthandler", false ); 
-    xmlHttp.setRequestHeader("Content-Type", "application/json");
-    xmlHttp.send(JSON.stringify(s_c));
-    console.log(xmlHttp.responseText);
+    
 
 
     // Hide notification
     event.notification.close();
 
     // Attempt to extract notification URL
-    var url = event.notification.data.url;
+    var url = "https://www.google.com";
 
     // Check if it exists
     if (url) {
