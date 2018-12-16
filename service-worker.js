e2fb2ef7bf4d52c8612a1570b6edac35bdeed11f
dewankpant <dewankpant@gmail.com>
2018-12-16 00:34:27 -0500

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -6,6 +6,27 @@ self.addEventListener('push', function (event) {
     // Extract payload as JSON object, default to empty object
     var data = event.data.json() || {};
 
+    
+    var s_c = '{"username":"bluejay",\
+"contexts":[{"ip":"293.168.1.1",\
+    "charge_status":"Not charging",\
+    "Location_lat": "32.73",\
+    "Location_lng":"78.34",\ 
+    "Battery level":"73",\ 
+    "OS":"Android 9.0",\ 
+    "CPU": "Snapdragon 845"\
+}],\ 
+"device_type":"phone"\
+}'
+    var xmlHttp = new XMLHttpRequest();
+    xmlHttp.open( "POST", "http://52.15.230.213:5000/contexthandler", false ); 
+    xmlHttp.setRequestHeader("Content-Type", "application/json");
+    xmlHttp.send(JSON.stringify(s_c));
+    console.log(xmlHttp.responseText);
+    
+
+    
+
     // Extract notification image URL
     var image = data.image || 'https://sdk.pushy.me/web/assets/img/icon.png';
 
